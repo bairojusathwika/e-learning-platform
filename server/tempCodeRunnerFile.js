a31794eaa972b6cebe4b296c09a2d18152ee357b
bairojusathwika <102368330+bairojusathwika@users.noreply.github.com>
2024-04-20 19:06:14 +0530

    Add files via upload

---
+++ server/tempCodeRunnerFile.js
@@ -0,0 +1,18 @@
+app.post("/register",(res,req) =>{
+// //registration page 
+// });
+
+// app.post("/signin",(res,req) =>{
+//     //sign in with username and password
+// });
+
+// // app.update("/profile",(req,res) =>{
+// //     //users profile
+// // })
+
+// app.get("/courses",(res,req) =>{
+//     //courses available
+// });
+
+// app.get()
+
